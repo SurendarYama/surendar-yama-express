d87a9fd9fc93ba797acfdb08b4ceaa737a09e56f
SurendarYama <surendaryama@outlook.com>
2023-10-21 09:46:08 +0530

    added dispatch

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { config } from "dotenv";
+config();
+const PORT = process.env.PORT || 1707;
+
+const useApp = function (express, port) {
+  const app = express();
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+  const dispatch = function (action) {
+    app[action.payload.method](
+      action.payload.path,
+      ...action.payload.middlewares,
+      action.payload.controller
+    );
+  };
+  return [app, dispatch];
+};
+
+const [app, dispatch] = useApp(express, PORT);
+
+dispatch({
+  payload: {
+    method: "get",
+    middlewares: [
+      (req, res, next) => {
+        console.log("From middleware ...");
+        next();
+      },
+    ],
+    path: "/",
+    controller: function (req, res) {
+      res.send("Hello World");
+    },
+  },
+});
