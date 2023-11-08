812e34f431ef4a1cc12b1c4d24d7904af71d504d
SurendarYama <surendaryama@outlook.com>
2023-11-08 13:25:54 +0530

    use and set helper methods support added

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-import { bootApp } from "./boot.js";
 import { config } from "dotenv";
+import { bootApp } from "./boot.js";
 import { endpoints } from "./app_modules/index.js";
-
 config();
-export const dispatch = bootApp();
 
+export const dispatch = bootApp();
 dispatch({
   type: "👽",
   payload: {
     port: process.env.PORT,
   },
 });
+
+function use(configs) {
+  for (let i = 0; i < configs.length; i++) {
+    dispatch({ type: "🦕", payload: { middlewares: configs[i] } });
+  }
+}
+
+function set(configs) {
+  for (let i = 0; i < configs.length; i++) {
+    dispatch({ type: "👑", payload: configs[i] });
+  }
+}
 
 function make(endpoints) {
   for (let i = 0; i < endpoints.length; i++) {
